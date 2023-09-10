b9f17d7bb021a9f6b8029977e8f7c66b28e556cc
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-09-10 05:34:26 -0300

    Atualizar o api.js

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://ojzhcxrufolkyqfgdwmi.supabase.co/rest/v1',
+    baseURL: SUPABASE_URL,
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qemhjeHJ1Zm9sa3lxZmdkd21pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDMwMzQ1OSwiZXhwIjoyMDA5ODc5NDU5fQ.akD5buAjTZlZEX97_sDupwYAZs3VC33k-M6w6EDmXAo",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qemhjeHJ1Zm9sa3lxZmdkd21pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDMwMzQ1OSwiZXhwIjoyMDA5ODc5NDU5fQ.akD5buAjTZlZEX97_sDupwYAZs3VC33k-M6w6EDmXAo"
+        apikey: SUPABASE_KEY,
+        authorization: SUPABASE_AUTH
     }
 })
